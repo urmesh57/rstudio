0039f78cfc55fb464fe72ec60f81ab4d178fdabb
Urmesh <urmesh3558@gmail.com>
2021-04-13 02:17:39 +0530

    read xls

--- first_file.R
+++ first_file.R
@@ -4,4 +4,11 @@ var1 <- "myself new"
 print(var1)
 typeof(var1)
 class(var1)
-
+hello_string <- 'hello'
+hello_string
+a= TRUE
+a
+##Read csv
+employee_1 <- read_xlsx("C:\\Users\\HP\\Desktop\\New folder\\datasets\\employee_1.xlsx")
+employee_1
+view
