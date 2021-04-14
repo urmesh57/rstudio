020d1074210d0fd3e4ee4ddfe4990733a2608da9
Urmesh <urmeh3558@gmail.com>
2021-04-14 18:54:06 +0530

    3

--- first_file.R
+++ first_file.R
@@ -8,11 +8,11 @@ hello_string <- 'hello'
 hello_string
 a= TRUE
 a
-##Read csv
+##Read csv with a filtered output
 employee_1 <- read_xlsx("C:\\Users\\urmen\\OneDrive\\Desktop\\dataasets\\Employee_details.xlsx")
 employee_1;
 View(employee_1)
 X=100
-x <- employee_1[employee_1$Dept=='HR',]
+x <- employee_1[employee_1$Dept=='HR' & employee_1$Pay>10005 ,]
 x
  
